3083b3be9aa44f8275a3cba7b5657bbe0f9715e7
PukiPuki <dfwong5566@gmail.com>
2017-10-15 12:50:43 +0000

    fix

--- screept.sh
+++ screept.sh
@@ -1,6 +1,7 @@
-mkfs.btrfs -L root /dev/sda
+mkfs.btrfs -L root -f /dev/sda
 mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=lzo,space_cache /dev/sda /mnt/
 btrfs subvolume create /mnt/nixos
+umount /mnt/
 mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=lzo,space_cache,subvol=nixos /dev/sda /mnt/
 btrfs subvolume create /mnt/var
 btrfs subvolume create /mnt/home
@@ -8,4 +9,3 @@ btrfs subvolume create /mnt/tmp
 
 nixos-generate-config --root /mnt/
 vim /mnt/etc/nixos/configuration.nix
-
