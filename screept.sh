e7166f87b0b5642699fd65fcc75764e6f4cf2b08
PukiPuki <dfwong5566@gmail.com>
2017-10-15 12:41:11 +0000

    sdb to sda

--- screept.sh
+++ screept.sh
@@ -1,7 +1,7 @@
 mkfs.btrfs -L root /dev/sda
 mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=lzo,space_cache /dev/sda /mnt/
 btrfs subvolume create /mnt/nixos
-mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=lzo,space_cache,subvol=nixos /dev/sdb /mnt/
+mount -t btrfs -o noatime,discard,ssd,autodefrag,compress=lzo,space_cache,subvol=nixos /dev/sda /mnt/
 btrfs subvolume create /mnt/var
 btrfs subvolume create /mnt/home
 btrfs subvolume create /mnt/tmp
